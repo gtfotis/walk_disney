7a6af546d8706deb3f07f4a1df371740fdd205fc
Sarah dePalo <sarahdepalo99@gmail.com>
2021-07-06 14:00:46 -0400

    added seed file and data

--- sql/seed.sql
+++ sql/seed.sql
@@ -2,3 +2,28 @@ INSERT INTO users
     (first_name, last_name, user_email, user_password)
 VALUES
     ('Mickey', 'Mouse', 'mickey@disney.com', 'password');
+
+INSERT INTO parks
+    (park_name, park_description)
+VALUES
+    ('Magic Kingdom Park', 'The most magical place on Earth! Magical Kingdom containes thrilling coasters and classic attractions.');
+
+INSERT INTO activities
+    (activity_name, activity_description, kid_friendly, activity_time, park_id)
+VALUES 
+    ('Happily Ever After', 'A grand finale to your day! The best fireworks display around.', true, '9:15', 1);
+
+INSERT INTO food
+    (restaurant_name, cuisine, price_range, park_id)
+VALUES
+    ('Aloha Isle', 'American', '$', 1);
+
+INSERT INTO lodging
+    (lodging_name, lodging_url, lodging_image, park_id)
+VALUES
+    ('The Cabins at Fort Wilderness Resort', 'https://disneyworld.disney.go.com/resorts/cabins-at-fort-wilderness-resort/rates-rooms/', '../imgs/wilderness_resort.jpeg', 1 );
+
+INSERT INTO plan 
+    (user_id, parks_id, activity_id, food_id, lodging_id)
+VALUES 
+    (1, 1, 1, 1, 1)
